8db27b07c9a2e94bcdc151efc3143cd124ebe973
Matano <mwakimamatano@gmail.com>
2023-12-17 20:24:40 +0300

    Add cpp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
 
